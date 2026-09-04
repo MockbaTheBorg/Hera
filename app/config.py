5b7795d3e987326bc8f8d6b27fedc85865de63a1
Mockba the Borg <mockba.the.borg@gmail.com>
2026-09-03 23:45:45 -0400

    Bumped version to 1.2

--- app/config.py
+++ app/config.py
@@ -13,7 +13,7 @@ import re
 from pathlib import Path
 
 VERSION_MAJOR = 1
-VERSION_MINOR = 1
+VERSION_MINOR = 2
 
 CONFIG_DIR = Path.home() / ".config" / "hera"
 CONFIG_FILE = CONFIG_DIR / "hera.conf"
